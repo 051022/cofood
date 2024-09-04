97f1cd1214f46d768c33bccc1fc6ca1cb76b83fc
051022 <2192186896@qq.com>
2024-09-04 23:27:50 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+
+export default {
+  base: '/cofood/', // 确保这个路径与 GitHub Pages 部署路径匹配
+}
