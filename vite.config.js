f3b88a14504c0d1c884e7462dd81ba709d6d8d0d
051022 <2192186896@qq.com>
2024-09-05 16:01:53 +0800

    Deploy updates

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,5 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
+  plugins: [vue()]
 });
