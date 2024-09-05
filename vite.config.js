98a0582375ce1c43125ba7514c39c9934b8481d3
051022 <2192186896@qq.com>
2024-09-05 15:47:24 +0800

    Update dependencies and configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-
-export default {
-  base: '/cofood/', // 确保这个路径与 GitHub Pages 部署路径匹配
-}
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
