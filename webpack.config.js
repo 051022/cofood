2ca33a504b532403cc24b7244ff0a87d8c7915a6
051022 <2192186896@qq.com>
2024-09-05 16:48:36 +0800

    Deploy updates

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-browser.js'
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+};
